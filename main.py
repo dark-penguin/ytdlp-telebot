341ecba6130e0055a58c4c0d8ce1e37685c0b822
darkpenguin <darkpenguin@posteo.de>
2024-12-28 18:27:09 +0400

    Use logging for logs instead of print Fix bugs with playlist pre-querying and some videos not downloading

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 import os
+import sys
 import re
 import signal
+import json
+import logging
 from datetime import datetime
 from dotenv import load_dotenv
 
 import telebot
 from yt_dlp import YoutubeDL, utils
 
-import json
+
+logging.basicConfig(format="%(levelname)s:%(module)s: %(message)s")
+logger = logging.getLogger(__name__)  # This logger's name does not matter!
+logger.setLevel(logging.DEBUG if "--debug" in sys.argv else logging.WARNING)
 
 
 load_dotenv()  # Load envvars from .env
 
 token = os.environ.get('TOKEN', None)
 if not token:
-    print("ERROR: Specify TOKEN in an .env file or an environment variable!")
+    logger.error("ERROR: Specify TOKEN in an .env file or an environment variable!")
     exit(1)
 log_channel = os.environ.get('LOG_CHANNEL', None)
 regex = os.environ.get('REGEX', r'\bhttps?://\S*\b')
@@ -51,9 +57,6 @@ options = {'format_sort': ['codec:h265:h264:h263'],
                               ],
 
            # Options set by default for the commandline (but not for the API!)
-           # 'extract_flat': True,  # Get only top-level info (do not go into resolving playlists/redirects)
-           # 'extract_flat': 'in_playlist',  # Do not process for playlists!
-           # This has to stay off, because otherwise it just reports "oh, it's an URL redirect somewhere"!
            # 'ignoreerrors': 'only_download',  # CLI default: 'only_download', API default: False
            'retries': 10,
            'fragment_retries': 10,
@@ -69,7 +72,7 @@ options.update({'format': formats})
 try:
     bot = telebot.TeleBot(token)
 except Exception as err:
-    print(f"Failed to register the bot!\n{err}")
+    logger.error(f"Failed to register the bot!\n{err}")
     exit(1)
 
 
@@ -93,31 +96,41 @@ def check_message(message):
 
     one_video_sent = False  # Hopefully this if per-invocation (concurrent-safe)!..
     for url in matches:
+        logger.info(f"-- Downloading video {matches.index(url)+1} of {len(matches)}")
+
         timestamp = f"{datetime.now():%Y-%m-%d_%H:%M:%S}"  # 2024-12-27 14:30:15
         filename = f'{timestamp}.%(ext)s'
 
         try:
-            # Quick check if this is a playlist to give the error sooner
-            with YoutubeDL(dict(options, outtmpl=filename, extract_flat=True, listformats=True)) as ydl:
+            # Quick check if this is a playlist - we don't want to accidentally download it
+            # If we download a playlist non-flatly, it will throw errors about deleted videos!
+            logger.info('-- First query: "flat" general information to check for playlist')
+            with YoutubeDL(dict(options, outtmpl=filename, extract_flat=True)) as ydl:
                 info = ydl.sanitize_info(ydl.extract_info(url, download=False))
-
-            # If this is a playlist, abort quietly
+            # It is possible that we get "Requested format not available" right here!
+            # Then we fall into "except:", and 'info' is still unassigned!
             if info.get('entries') or (info.get('_type') == "playlist"):
-                continue
+                continue  # If this is a playlist, abort quietly
 
-            # Try to download it (I wonder if it could still be a playlist behind an URL redirect...)
-            with YoutubeDL(dict(options, outtmpl=filename)) as ydl:
+            logger.info('-- Second query: attempt to download the video')
+            with YoutubeDL(dict(options, outtmpl=filename, extract_flat=False)) as ydl:
                 info = ydl.sanitize_info(ydl.extract_info(url, download=True))
 
-        except utils.DownloadError as error:  # Note that you can't handle errors "normally"!
+        except utils.DownloadError as error:  # Note that you can't handle errors "normally" in yt-dlp!
             # Check for UnsupportedError first, because it is also an ExtractorError!
             if isinstance(error.exc_info[1], utils.UnsupportedError):
                 continue
             # Check this with "elif", or continue, because otherwise both will match!
             elif isinstance(error.exc_info[1], utils.ExtractorError):
+                # !! NOTE that 'info' might still be unassigned!
                 # We'd like to see what formats are available
+                with YoutubeDL(dict(options, outtmpl=filename, extract_flat=False, listformats=True)) as ydl:
+                    info = ydl.sanitize_info(ydl.extract_info(url, download=False))
+
+
                 # TODO: The output is too large for Telegram - should process it first!
                 # TODO: DEBUG - how the fuck do I process this JSON?..
+                logger.info('-- Third query: extract formats')
                 with open("/tmpfs/test.log", 'w') as file:
                     json.dump(info.get('formats'), file, indent=4)
                 log(f"{error}\n\n{type(error.exc_info[1])}\n\nFormats available: <WIP>\n\n{url}")
@@ -145,7 +158,8 @@ def check_message(message):
             try:
                 # Repost the whole original text in the first message; in other messages, only post the URL
                 bot.send_video(message.chat.id, file, reply_to_message_id=message.id,
-                               caption=message.text if not one_video_sent else url)
+                               caption=url if one_video_sent else message.text)
+                one_video_sent = True
                 os.remove(filename)
             except Exception as error:
                 log(f"{error}\n\n{url}")
@@ -155,6 +169,7 @@ def check_message(message):
 
 
 def stop(sig, _):
+    # Better print this because it's not a "warning/error" level but we want to see it
     print(f"Caught {signal.Signals(sig).name}, exiting gracefully...")
     bot.stop_bot()
     exit(0)
